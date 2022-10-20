6f4e434e7732380938b80bb91f4f71a6e102e6b7
MilenaRadeva03 <116205054+MilenaRadeva03@users.noreply.github.com>
2022-10-21 00:57:54 +0300

    Ex7.cpp

---
+++ Ex7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    int counter = 0;
+    for (int i = 1; i <= n;i++)
+    {
+        if (i % 3 == 0 || i % 5 == 0 )
+        {
+            counter++;
+        } 
+    }
+    std::cout<< counter;
+}
